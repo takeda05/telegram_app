df3ca34abc4000192d9515cd459937ed34de854f
asadbek <asadbekzaynobiddinov51@gmail.com>
2024-08-11 11:52:45 +0500

    Dastlabgi ko" rinish tayyor

---
+++ main.py
@@ -0,0 +1,116 @@
+from PyQt5.QtWidgets import *
+import mysql.connector
+from stylesheet import *
+
+
+def get_connection():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='k7119zamu',
+            database='telegram'
+        )
+        return connection
+    except mysql.connector.Error as e:
+        print('Xato sodir bo‘ldi:', e)
+
+
+class LoginWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.logins = list()
+
+        self.setMinimumSize(400, 500)
+
+        login_label = QLabel('Login')
+        password_label = QLabel("Password")
+
+        login_label.setMinimumWidth(100)
+        password_label.setMinimumWidth(100)
+
+        ok_btn = QPushButton('Ok')
+
+        self.login_line = QLineEdit()
+        self.password_line = QLineEdit()
+
+        self.password_line.setEchoMode(QLineEdit.Password)
+
+        login_layout = QHBoxLayout()
+        login_layout.addWidget(login_label)
+        login_layout.addWidget(self.login_line)
+
+        password_layout = QHBoxLayout()
+        password_layout.addWidget(password_label)
+        password_layout.addWidget(self.password_line)
+
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(login_layout)
+        main_layout.addLayout(password_layout)
+        main_layout.addWidget(ok_btn)
+
+        self.login_line.setStyleSheet(login_line_style)
+        self.password_line.setStyleSheet(password_line_style)
+        login_label.setStyleSheet(label_style)
+        password_label.setStyleSheet(label_style)
+        ok_btn.setStyleSheet(button_style)
+
+        ok_btn.clicked.connect(self.open_new_window)
+
+        widget = QWidget()
+        widget.setLayout(main_layout)
+
+        self.setCentralWidget(widget)
+
+    def open_new_window(self):
+        new_window = NewWindow()
+        new_window.show()
+        self.logins.append(new_window)
+
+
+class NewWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.setMinimumSize(700, 900)
+
+        self.receiver = QLineEdit()
+        self.receiver.setPlaceholderText('Qabul qiluvchi nickneymini kiriting: ')
+
+        self.inchat = QTextEdit()
+        self.outchat = QTextEdit()
+
+        inchat_label = QLabel('Sizga kelgan habar')
+        outchat_label = QLabel('Siz yuboradigan habar')
+
+        send_btn = QPushButton('Send')
+
+        inchat_layout = QVBoxLayout()
+        inchat_layout.addWidget(inchat_label)
+        inchat_layout.addWidget(self.inchat)
+
+        outchat_layout = QVBoxLayout()
+        outchat_layout.addWidget(outchat_label)
+        outchat_layout.addWidget(self.outchat)
+
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.receiver)
+        main_layout.addLayout(inchat_layout)
+        main_layout.addLayout(outchat_layout)
+        main_layout.addWidget(send_btn)
+
+        self.receiver.setStyleSheet(receiver_style)
+        self.inchat.setStyleSheet(text_edit_style)
+        self.outchat.setStyleSheet(text_edit_style)
+        inchat_label.setStyleSheet(label_style)
+        outchat_label.setStyleSheet(label_style)
+        send_btn.setStyleSheet(button_style)
+
+        self.setLayout(main_layout)
+
+
+app = QApplication([])
+main = LoginWindow()
+main.show()
+app.exec_()
---
+++ stylesheet.py
@@ -0,0 +1,61 @@
+
+login_line_style = """
+QLineEdit {
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+QLineEdit:focus {
+    border-color: #0078d7;
+}
+"""
+
+password_line_style = """
+QLineEdit {
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+QLineEdit:focus {
+    border-color: #0078d7;
+}
+"""
+
+label_style = "font-size: 18px;"
+
+button_style = """
+QPushButton {
+    background-color: #0078d7;
+    color: white;
+    padding: 10px 20px;
+    border-radius: 5px;
+}
+QPushButton:hover {
+    background-color: #005bb5;
+}
+QPushButton:pressed {
+    background-color: #004494;
+}
+"""
+
+receiver_style = """
+QLineEdit {
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+QLineEdit:focus {
+    border-color: #0078d7;
+}
+"""
+
+text_edit_style = """
+QTextEdit {
+    padding: 10px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+QTextEdit:focus {
+    border-color: #0078d7;
+}
+"""
