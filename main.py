817b05d53ec407def9cd32dbb527ed4101686d35
asadbek <asadbekzaynobiddinov51@gmail.com>
2024-08-11 12:50:08 +0500

    Proyect bitti

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def get_connection():
         )
         return connection
     except mysql.connector.Error as e:
-        print('Xato sodir bo‘ldi:', e)
+        QMessageBox.critical(None, 'Xato', f'Ma\'lumotlar bazasiga ulanishda xato: {e}')
+        return None
 
 
 class LoginWindow(QMainWindow):
@@ -68,17 +69,23 @@ class LoginWindow(QMainWindow):
         password = self.password_line.text()
 
         conn = get_connection()
-        curs = conn.cursor()
+        if conn is None:
+            return
 
-        curs.execute('SELECT * FROM whatsapp WHERE login = %s AND password = %s', (login, password))
-        informations = curs.fetchone()
-
-        curs.close()
-        conn.close()
+        try:
+            curs = conn.cursor()
+            curs.execute('SELECT * FROM whatsapp WHERE login = %s AND password = %s', (login, password))
+            informations = curs.fetchone()
+        except mysql.connector.Error as e:
+            QMessageBox.critical(self, 'Xato', f'Ma\'lumotlar bazasidan ma\'lumot olishda xato: {e}')
+            return
+        finally:
+            curs.close()
+            conn.close()
 
         if informations:
-            print('Login muvaffaqiyatli amalga oshirildi.')
-            new_window = NewWindow()
+            new_window = NewWindow(login)
+            new_window.inchat.setText(informations[2])
             new_window.show()
             self.logins.append(new_window)
         else:
@@ -87,21 +94,24 @@ class LoginWindow(QMainWindow):
 
 
 class NewWindow(QWidget):
-    def __init__(self):
+    def __init__(self, login):
         super().__init__()
 
+        self.login = login
         self.setMinimumSize(700, 900)
 
         self.receiver = QLineEdit()
-        self.receiver.setPlaceholderText('Qabul qiluvchi nickneymini kiriting: ')
+        self.receiver.setPlaceholderText(f'Qabul qiluvchi nickneymini kiriting ({self.login} tomonidan):')
 
         self.inchat = QTextEdit()
+        self.inchat.setReadOnly(True)
         self.outchat = QTextEdit()
 
         inchat_label = QLabel('Sizga kelgan habar')
         outchat_label = QLabel('Siz yuboradigan habar')
 
         send_btn = QPushButton('Send')
+        send_btn.clicked.connect(self.send_message)
 
         inchat_layout = QVBoxLayout()
         inchat_layout.addWidget(inchat_label)
@@ -124,12 +134,42 @@ class NewWindow(QWidget):
         outchat_label.setStyleSheet(label_style)
         send_btn.setStyleSheet(button_style)
 
-        send_btn.clicked.connect(self.send_message)
-
         self.setLayout(main_layout)
 
     def send_message(self):
-        ...
+        message = self.outchat.toPlainText()
+        receiver = self.receiver.text()
+
+        if not message.strip():
+            QMessageBox.warning(self, 'Xato', 'Yuboriladigan habar bo\'sh bo\'lmasligi kerak.')
+            return
+
+        if not receiver.strip():
+            QMessageBox.warning(self, 'Xato', 'Qabul qiluvchi nickneymini kiriting.')
+            return
+
+        conn = get_connection()
+        if conn is None:
+            return
+
+        try:
+            curs = conn.cursor()
+            curs.execute('SELECT * FROM whatsapp WHERE login = %s', (receiver,))
+            receiver_info = curs.fetchone()
+
+            if receiver_info:
+                curs.execute('update whatsapp set inchat = %s where login = %s', (message, receiver))
+                conn.commit()
+                self.outchat.clear()
+
+                QMessageBox.information(self, 'Muvaffaqiyat', 'Xabar muvaffaqiyatli yuborildi.')
+            else:
+                QMessageBox.warning(self, 'Xato', 'Qabul qiluvchi login mavjud emas.')
+        except mysql.connector.Error as e:
+            QMessageBox.critical(self, 'Xato', f'Ma\'lumotlar bazasiga ulanishda xato: {e}')
+        finally:
+            curs.close()
+            conn.close()
 
 
 app = QApplication([])
